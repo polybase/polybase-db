d2f5c442adc9a7a844b49b444b4001401e67e412
Cal <calum.moore@gmail.com>
2023-02-02 16:56:36 +0100

    Add docker

--- polybase/src/main.rs
+++ polybase/src/main.rs
@@ -10,7 +10,7 @@ async fn root() -> impl Responder {
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(root))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
