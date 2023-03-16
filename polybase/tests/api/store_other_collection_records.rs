6e64469f89c26b5d2d2a107ce3f79c398b3d4f5c
Mateusz Mielewczyk <mateuszmlc@protonmail.com>
2023-03-16 18:02:13 +0100

    Add a test for passing and storing arrays of collection records

--- polybase/tests/api/store_other_collection_records.rs
+++ polybase/tests/api/store_other_collection_records.rs
@@ -130,3 +130,93 @@ collection User {
         }
     );
 }
+
+#[tokio::test]
+async fn from_array_argument() {
+    let server = Server::setup_and_wait().await;
+
+    let schema = r#"
+@public
+collection Account {
+    id: string;
+    managers: User[];
+
+    constructor (id: string, managers: User[]) {
+        this.id = id;
+        this.managers = managers;
+    }
+}
+
+@public
+collection User {
+    id: string;
+
+    constructor (id: string) {
+        this.id = id;
+    }
+}
+    "#;
+
+    #[derive(Debug, PartialEq, serde::Serialize, serde::Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    struct Account {
+        id: String,
+        managers: Vec<ForeignRecordReference>,
+    }
+
+    #[derive(Debug, PartialEq, serde::Serialize, serde::Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    struct User {
+        id: String,
+    }
+
+    let account_collection = server
+        .create_collection::<Account>("test/Account", schema, None)
+        .await
+        .unwrap();
+
+    let user_collection = server
+        .create_collection::<User>("test/User", schema, None)
+        .await
+        .unwrap();
+
+    assert_eq!(
+        user_collection.create(json!(["id1"]), None).await.unwrap(),
+        User {
+            id: "id1".to_string(),
+        }
+    );
+
+    assert_eq!(
+        user_collection.create(json!(["id2"]), None).await.unwrap(),
+        User {
+            id: "id2".to_string(),
+        }
+    );
+
+    assert_eq!(
+        account_collection
+            .create(
+                json!(["id1", [
+                    { "collectionId": &user_collection.id, "id": "id1" },
+                    { "collectionId": &user_collection.id, "id": "id2" },
+                ]]),
+                None
+            )
+            .await
+            .unwrap(),
+        Account {
+            id: "id1".to_string(),
+            managers: vec![
+                ForeignRecordReference {
+                    collection_id: user_collection.id.clone(),
+                    id: "id1".to_string(),
+                },
+                ForeignRecordReference {
+                    collection_id: user_collection.id.clone(),
+                    id: "id2".to_string(),
+                }
+            ],
+        }
+    );
+}
