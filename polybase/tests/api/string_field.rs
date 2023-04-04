aa6a90088c71f776609ae2f436e61ef95e0f8992
Cal <calum.moore@gmail.com>
2023-04-04 20:45:38 +0100

    Add test for static strings

---
+++ polybase/tests/api/string_field.rs
@@ -0,0 +1,49 @@
+use serde_json::json;
+
+use crate::api::{ListQuery, Server};
+
+#[tokio::test]
+async fn collection_string_field() {
+    let server = Server::setup_and_wait().await;
+
+    let schema = r#"
+@public
+collection Account {
+    id: string;
+    hello: string;
+    world: boolean;
+
+    @index(name, isActive);
+
+    constructor (id: string) {
+        this.id = id;
+        this.hello = "hello";
+        this.world = 'world';
+    }
+}
+    "#;
+
+    #[derive(Debug, PartialEq, Clone, serde::Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    struct Account {
+        id: String,
+        hello: String,
+        world: String,
+    }
+
+    let collection = server
+        .create_collection::<Account>("ns/Account", schema, None)
+        .await
+        .unwrap();
+
+    let account_id1_john_true = collection.create(json!(["id1"]), None).await.unwrap();
+
+    assert_eq!(
+        account_id1_john_true,
+        Account {
+            id: "id1".to_string(),
+            hello: "hello".to_string(),
+            world: "world".to_string(),
+        }
+    );
+}
