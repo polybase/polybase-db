d3a912f997377e9145469b65ca535cc84502ad77
Cal <calum.moore@gmail.com>
2023-05-31 11:55:15 +0100

    Emit structured logs (#103)

--- polybase/src/errors/metrics.rs
+++ polybase/src/errors/metrics.rs
@@ -1,10 +1,26 @@
 //! This module handles various metrics data returned from handlers and meant to be
 //! processed in the logger middleware.
 
+use slog::{Record, Result, Serializer, KV};
 use std::fmt;
 
 pub enum MetricsData {
     NumberOfRecordsBeingReturned { req_uri: String, num_records: usize },
+}
+
+impl KV for MetricsData {
+    fn serialize(&self, _rec: &Record, serializer: &mut dyn Serializer) -> Result {
+        match self {
+            MetricsData::NumberOfRecordsBeingReturned {
+                req_uri,
+                num_records,
+            } => {
+                serializer.emit_str("req_uri", req_uri)?;
+                serializer.emit_usize("num_records", *num_records)?;
+            }
+        }
+        Ok(())
+    }
 }
 
 impl fmt::Display for MetricsData {
@@ -13,10 +29,7 @@ impl fmt::Display for MetricsData {
             f,
             "{}",
             match self {
-                MetricsData::NumberOfRecordsBeingReturned {
-                    req_uri,
-                    num_records,
-                } => format!("request uri = {req_uri}, number of records = {num_records}"),
+                MetricsData::NumberOfRecordsBeingReturned { .. } => "request_response_metrics",
             }
         )
     }
